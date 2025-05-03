a24df1d195d11cef00fa22d43e208a4ef422b1f6
Garmah <PiotrekG000@gmail.com>
2025-05-03 10:53:23 +0200

    Updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-	base:'https://github.com/Garmah98/LandingPage/',
+	base:'/LandingPage/',
 	plugins: [react(), tailwindcss()],
 });
