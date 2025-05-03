71cd562f4f62c635d090eaa38655a6741051c22b
Garmah <PiotrekG000@gmail.com>
2025-05-03 10:22:43 +0200

    Added responsive mobile version

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -6,7 +6,7 @@ type PageProps = {
 
 export default function Page({ children, className }: PageProps) {
     let styles =
-        ' absolute top-1/2 h-[90%] w-[45%] -translate-y-1/2 shadow-lg shadow-black ring-3 ring-white animate-fadeIn'
+        ' md:absolute md:top-1/2 w-full h-screen md:h-[90%] md:w-[45%] md:-translate-y-1/2 shadow-lg shadow-black ring-3 ring-white animate-fadeIn'
     styles = className + styles
 
     return <section className={styles}>{children}</section>
